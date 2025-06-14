514ffb91e4a1514894f86d9ca6d8e9bc0d199a62
Alice Carroll <git@alice-carroll.pet>
2025-06-14 20:01:12 +0300

    chore: update inputs

--- default.nix
+++ default.nix
@@ -1,13 +1,9 @@
 {
   rustPlatform,
-  lib,
-  darwin,
-  stdenv,
 }:
 rustPlatform.buildRustPackage {
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;
   pname = "mole";
   version = "0.0.1";
-  buildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.IOKit;
 }
--- flake.nix
+++ flake.nix
@@ -1,35 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{
       flake-parts,
       nixpkgs,
-      rust-overlay,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.platforms.all;
       perSystem =
         {
-          config,
-          self',
-          inputs',
           pkgs,
-          lib,
-          system,
           ...
         }:
         {
           packages.default = pkgs.callPackage ./default.nix { };
-          devShells.default = pkgs.mkShell {
-            buildInputs = lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ IOKit ]);
-          };
         };
     };
 }
